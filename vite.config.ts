193d359ca12cfbd3f6b04a3e8deb6e5ef308f856
Dipanshu Bisht <dipanshu.bisht@gmail.com>
2025-07-22 22:40:39 +0530

    Fix viteConfig

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from "@vitejs/plugin-react-swc";
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: true,
+    allowedHosts: true,
     port: 5173,
   },
   preview: {
